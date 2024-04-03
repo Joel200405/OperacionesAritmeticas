acc84d0843589ddd390d93adaa019dfe58a403f0
Joel200405 <74091904@continental.edu.pe>
2024-04-03 18:34:13 -0500

    Se quitó las validaciones

--- main.py
+++ main.py
@@ -3,37 +3,15 @@ def calcular_mcd(a, b):
         a, b = b, a % b
     return a
 
-# Función para validar si la entrada es un entero positivo
-def validar_entero_positivo(valor):
-    try:
-        valor_entero = int(valor)
-        if valor_entero <= 0:
-            raise ValueError("El número debe ser positivo y diferente de cero.")
-        return valor_entero
-    except ValueError:
-        raise ValueError("Debe ingresar un número entero válido.")
-
-# Solicitar al usuario que ingrese los números y validarlos
-while True:
-    try:
-        num1 = input("Ingrese el primer número: ")
-        num1 = validar_entero_positivo(num1)
-        break
-    except ValueError as error:
-        print(error)
-
-while True:
-    try:
-        num2 = input("Ingrese el segundo número: ")
-        num2 = validar_entero_positivo(num2)
-        break
-    except ValueError as error:
-        print(error)
+# Solicitar al usuario que ingrese los números
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
 
 # Calcular el MCD utilizando la función definida anteriormente
 mcd = calcular_mcd(num1, num2)
 
 # Mostrar el resultado
 print("El Máximo Común Divisor de", num1, "y", num2, "es:", mcd)
+
 
 
